cb4b7521ce0faa32c5d1a20142e0fcf96d8371c5
Field-general <dshitote@gmail.com>
2022-12-08 14:15:34 -0500

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include studio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{ 
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+
+}
